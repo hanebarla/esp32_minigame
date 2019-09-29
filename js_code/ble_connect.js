fbcd3ebfa3de1d4fef80c610fa8de92b41a16267
羽原丈博 <takehiro@habaratakehironoMacBook-puro.local>
2019-09-29 14:12:34 +0900

    connection test

---
+++ js_code/ble_connect.js
@@ -0,0 +1,29 @@
+//CONNECTボタン
+function connect(){
+    navigator.bluetooth.requestDevice({
+        filters: [{
+            services: [
+                '2222'
+            ]
+        }]
+    })
+    .then(device =>{
+        return device.connectGATT();
+    })
+    .then(server =>{
+        return Promise.all([
+            server.getPrimaryService('2222')
+        ]);
+    })
+    .then(services =>{
+        return Promise.all([
+            services[0].getCharcteristic('connection_test')
+        ]);
+    })
+    .then(characteristics =>{
+
+    })
+    .catch(error =>{
+        console.log(error);
+    })
+}
