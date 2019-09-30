b7fa11ae1174511c2f815feb78540572d86f2e10
hanebarla <49979598+hanebarla@users.noreply.github.com>
2019-09-30 23:19:59 +0900

    Update minigame.js

--- js_code/minigame.js
+++ js_code/minigame.js
@@ -5,10 +5,15 @@ if(!PIXI.utils.isWebGLSupported()){
 PIXI.utils.sayHello(type);
 
 var app = new PIXI.Application({
-    width: 600,
-    height: 800
+    width: 256,
+    height: 256
 });
 
 document.body.appendChild(app.view);
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoDensity = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
 //var stage = new PIXI.Container();
