1868b7367f475356e322793371744013478fa1c9
羽原丈博 <takehiro@habaratakehironoMacBook-puro.local>
2019-09-30 23:14:09 +0900

    pixijs 4

--- js_code/minigame.js
+++ js_code/minigame.js
@@ -5,15 +5,10 @@ if(!PIXI.utils.isWebGLSupported()){
 PIXI.utils.sayHello(type);
 
 var app = new PIXI.Application({
-    width: 256,
-    height: 256
+    width: 600,
+    height: 800
 });
 
 document.body.appendChild(app.view);
-
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.autoDensity = true;
-app.renderer.resize(window.innerWidth, window.innerHeight);
 
 //var stage = new PIXI.Container();
