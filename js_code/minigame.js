82d50000d88a168240e5ab2295cfd8755d6a4f29
hane hane <barla.wings@gmail.com>
2019-10-03 16:37:13 +0900

    ship_image

--- js_code/minigame.js
+++ js_code/minigame.js
@@ -1,22 +1,6 @@
 const rend_width = 400;
 const rend_height = 500;
 
-/*
-var w_width = window.innerWidth;
-var w_height = window.innerHeight;
-
-var width_scale = w_width / rend_width;
-var height_scale = w_height / rend_height;
-
-if(w_width >= w_height){
-    rend_width = rend_width * height_scale * 6/7;
-    rend_height = rend_height * 6/7;
-}else{
-    rend_width = rend_width;
-    rend_height = rend_height * rend_scale;
-}
-*/
-
 var type = "WebGL";
 if(!PIXI.utils.isWebGLSupported()){
     type = "canvas";
@@ -29,5 +13,15 @@ var app = new PIXI.Application({
 });
 
 document.body.appendChild(app.view);
+
+PIXI.loader
+    .add("images/ship.png")
+    .load(setup);
+
+function setup(){
+    var ship = new PIXI.Sprite(PIXI.loader.resources["images/ship.png"].texture);
+
+    app.stage.addChild(ship);
+}
 
 //var stage = new PIXI.Container();
