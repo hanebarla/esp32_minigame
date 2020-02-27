0113bda73dd96d91978471e1050d4e07fb4c4abb
hane <takehiro@habaratakehironoMacBook-puro.local>
2020-02-27 16:48:50 +0900

    Imcommplete collision

--- js_code/minigame.js
+++ js_code/minigame.js
@@ -4,6 +4,7 @@ let player_ship;
 let enemy_ship;
 let state;
 const speed = 1.5;
+let message = new Text();
 
 //WebGL support?
 let type = "WebGL";
@@ -22,8 +23,8 @@ document.getElementById("display").appendChild(app.view);
 //texture load
 PIXI.Loader.shared
     .add(["images/ship.png",
-          "images/my_ship.png",
-          "images/enemy_ship.png"])
+          "images/my_ship_huge.png",
+          "images/enemy_ship_huge.png"])
     .on("progress", loadProgressHandler)
     .load(setup);
 
@@ -33,8 +34,8 @@ function loadProgressHandler(){
 
 function setup(){
     //Create player and enemy ship
-    player_ship = new PIXI.Sprite(PIXI.Loader.shared.resources["images/my_ship.png"].texture);
-    enemy_ship = new PIXI.Sprite(PIXI.Loader.shared.resources["images/enemy_ship.png"].texture);
+    player_ship = new PIXI.Sprite(PIXI.Loader.shared.resources["images/my_ship_huge.png"].texture);
+    enemy_ship = new PIXI.Sprite(PIXI.Loader.shared.resources["images/enemy_ship_huge.png"].texture);
 
     player_ship.anchor.set(0.5);
     player_ship.x = app.screen.width / 2;
@@ -107,6 +108,10 @@ function gameloop(delta){
 }
 
 function play(delta){
+    if(hitTestRectangle(player_ship, enemy_ship)){
+        message.text = "hit!";
+        console.log("hit");
+    }
     player_ship.x += player_ship.vx;
     player_ship.y += player_ship.vy;
     player_ship.x = display_loop(player_ship.x, rend_width);
@@ -158,3 +163,61 @@ function keyboard(value){
 
     return key;
 }
+
+function hitTestRectangle(r1, r2) {
+
+    //Define the variables we'll need to calculate
+    let hit, combinedHalfWidths, combinedHalfHeights, vx, vy, leng, combinedleng;
+  
+    //hit will determine whether there's a collision
+    hit = false;
+  
+    //Find the center points of each sprite
+    r1.centerX = r1.x + r1.width / 2;
+    r1.centerY = r1.y + r1.height / 2;
+    r2.centerX = r2.x + r2.width / 2;
+    r2.centerY = r2.y + r2.height / 2;
+  
+    //Find the half-widths and half-heights of each sprite
+    r1.halfWidth = r1.width / 2;
+    r1.halfHeight = r1.height / 2;
+    r2.halfWidth = r2.width / 2;
+    r2.halfHeight = r2.height / 2;
+  
+    //Calculate the distance vector between the sprites
+    vx = Math.abs(r1.centerX - r2.centerX);
+    vy = Math.abs(r1.centerY - r2.centerY);
+    //leng = Math.sqrt(vx*vx + vy*vy);
+  
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = (r1.halfWidth + r2.halfWidth);
+    combinedHalfHeights = 2*(r1.halfHeight + r2.halfHeight)/3;
+    //combinedleng = Math.sqrt(combinedHalfHeights*combinedHalfHeights + combinedHalfWidths*combinedHalfWidths);
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+  
+      //A collision might be occurring. Check for a collision on the y axis
+      if (Math.abs(vy) < combinedHalfHeights) {
+  
+        //There's definitely a collision happening
+        hit = true;
+      } else {
+  
+        //There's no collision on the y axis
+        hit = false;
+      }
+    } else {
+  
+      //There's no collision on the x axis
+      hit = false;
+    }
+    /*
+    if(leng < combinedleng){
+        hit = true;
+    }else{
+        hit = false;
+    }*/
+  
+    //`hit` will be either `true` or `false`
+    return hit;
+  };
